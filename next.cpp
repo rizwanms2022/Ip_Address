4f1af240a33b6778b055e26e38c380240e8a01f7
Rizwan Ahmed <irizwanms@gmail.com>
2024-12-16 14:59:10 +0500

    Update next.cpp

--- next.cpp
+++ next.cpp
@@ -1,14 +1,1 @@
-#include <iostream>
-using namespace std;
 
-int main()
-{
-    cout<<"Rizwan AHmed\n";
-    cout<<"Rizwan Baloch\n";
-    for (size_t i = 0; i < 100; i++)
-    {
-        cout<<"Got it  ";
-    }
-    
-    return 0;
-}
